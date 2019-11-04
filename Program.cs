f6826f7256c714032c721d6ee0921737fa63760b
Anton K <kokoshkoa@gmail.com>
2019-11-05 00:55:54 +0200

    [npuzzle] todo-s

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace Npuzzle
 			var parser = new Parser();
 			List<string> strMap;
 
+			//read whole map
 			if(args.Length == 0)
 			{
 				strMap = parser.ReadFromConsole();
@@ -20,6 +21,7 @@ namespace Npuzzle
 				strMap = parser.ReadFromFile(args[0]);
 			}
 
+			//parsing
 			if (parser.Parse(strMap, out uint[,] mass))
 			{
 				Console.WriteLine("We got a mass");
@@ -29,6 +31,10 @@ namespace Npuzzle
 				Console.WriteLine("Some errors");
 			}
 
+
+			//TODO: check map validity e.g.: no number duplication, map can be solvet etc.
+
+
 			//do
 			//{ 
 			//	var str = Console.ReadLine();
@@ -36,8 +42,6 @@ namespace Npuzzle
 			//}
 			//while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 			//Console.ReadKey();
-
-
 		}
 	}
 }
